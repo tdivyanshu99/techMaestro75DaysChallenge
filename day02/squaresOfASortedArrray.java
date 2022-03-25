e6602bc3e6a1bef3ec7991fe142e3999e1c4ba81
Divyanshu Tiwari <tdivyanshu99@gmail.com>
2022-03-25 23:53:31 +0530

    added day 2 code

---
+++ day02/squaresOfASortedArrray.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] sortedSquares(int[] nums) {
+        int n=nums.length;
+        int[] ar=new int[n];
+        
+        int i=0,j=n-1,k=n-1;
+        while(i<=j){
+            if(nums[i]*nums[i]<nums[j]*nums[j]){
+                ar[k]=nums[j]*nums[j];
+                j--;
+                k--;
+            }
+            else{
+                ar[k]=nums[i]*nums[i];
+                i++;
+                k--;
+            }
+        }
+        return ar;
+    }
+}
